c9b565e0c3aaa900de63708439039deb54ad611c
Keeler Spear <keelerspear2@gmail.com>
2025-02-15 11:53:39 -0700

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,20 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        //y1' = y2
+        //y2' = 2y2 - y1
+
+        TriFunction<Double, Double, Double, Double> fnc1 = (t, y1, y2) -> y2;
+
+        TriFunction<Double, Double, Double, Double> fnc2 = (t, y1, y2) -> 2 * y2 - y1;
+
+        double[] y0 = {1, 1};
+
+        double[][] v = ODE.rk4System(fnc1, fnc2, 0, y0, 0.2, 0.1);
+
+        Matrix vals = new Matrix(v);
+
+        System.out.println(vals);
     }
 }
