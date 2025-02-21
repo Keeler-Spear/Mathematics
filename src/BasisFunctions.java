1e95bc650d7309b90548bfb2135ce62ad5b3a0a5
Keeler Spear <keelerspear2@gmail.com>
2025-02-21 15:39:06 -0700

    Made it so gradient descent can now model any set of basis functions!

---
+++ src/BasisFunctions.java
@@ -0,0 +1,70 @@
+import java.util.function.Function;
+
+/**
+ * A class that creates basis functions.
+ * <p>
+ *     This class and all functions within were added after I submitted my application to your institution.
+ * </p>
+ *
+ * @author Keeler Spear
+ * @version %I%, %G%
+ * @since 1.0
+ */
+public class BasisFunctions {
+
+    /**
+     * Creates a set of polynomial basis functions of the size specified.
+     *
+     * @param n The highest polynomial order in the basis function set.
+     * @throws IllegalArgumentException If the set's order is negative.
+     */
+    public static Function[] polynomials(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("The set's order must be at least zero!");
+        }
+
+        Function<Double, Double>[] fncs = new Function[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            double I = i;
+            fncs[i] = x -> Math.pow(x, I);
+        }
+
+        return fncs;
+    }
+
+    /**
+     * Creates a set of trigonometric basis functions of the size specified.
+     *
+     * @param n The highest polynomial order in the basis function set.
+     * @throws IllegalArgumentException If the set's order is less than two.
+     */
+    public static Function[] trig(int n) { //Orthogonal on [-PI, PI]
+        if (n < 2) {
+            throw new IllegalArgumentException("The set's order must be at least two!");
+        }
+
+        Function<Double, Double>[] fncs = new Function[n + 1];
+
+        double inPI = 1.0 / Math.sqrt(Math.PI);
+
+        double p0 = 1.0 / Math.sqrt(2 * Math.PI);
+        fncs[0] = x -> p0;
+        fncs[1] = x -> inPI * Math.sin(x);
+        fncs[2] = x -> inPI * Math.cos(x);
+
+        for (int i = 3; i <= n; i++) {
+            double I = i;
+            if (i % 2 == 1) { //i is odd
+                fncs[i] = x -> p0 * Math.sin(((I + 1.0) / 2.0) * x);
+            }
+            else { //i is even
+                fncs[i] = x -> p0 * Math.cos((I / 2.0) * x);
+            }
+            fncs[i] = x -> Math.pow(x, I);
+        }
+
+        return fncs;
+    }
+
+}
--- src/LinReg.java
+++ src/LinReg.java
@@ -1,8 +1,11 @@
+import java.util.function.Function;
+
 /**
  * A static class that performs the operations essential to machine learning.
  * <p>
  *     This class and all functions within were added after I submitted my application to your institution.
  * </p>
+ *
  * @author Keeler Spear
  * @version %I%, %G%
  * @since 1.0
@@ -26,7 +29,7 @@ public class LinReg {
      * @return A matrix with the weights for a polynomial regression model based on the data set provided.
      * @throws IllegalArgumentException If the data does not have one sample for each label.
      * @throws IllegalArgumentException If n is less than one.
-     * @see #polyGradDes(Matrix, Matrix, Matrix, int, double)
+     * @see #gradDes(Matrix, Matrix, Matrix, int, double, Function[])
      */
     public static Matrix polyGradDes(Matrix x, Matrix y, int n) {
         if (x.getRows() != y.getRows()) {
@@ -38,6 +41,7 @@ public class LinReg {
         }
 
         Matrix w = LinearAlgebra.randMatrix(n * x.getCols() + 1, 1, -RAND_BOUND, RAND_BOUND);
+        Function[] fncs = BasisFunctions.polynomials(n);
         double a;
 
         if (n <= 2) {
@@ -57,17 +61,52 @@ public class LinReg {
      * w + wx + wy + wz + ... + wx^2 + wx^3 + wx^n + ... + wy^2 + wy^3 + ... + wy^n + wz^2 + wz^3 + ... + wz^n + ...
      * </p>
      *
-     * @param xOrg A matrix of data parameters.
+     * @param x A matrix of data parameters.
      * @param y A vector of data labels.
      * @param w A vector of initial weight guesses.
      * @param n The order of the polynomial.
      * @param a The learning rate of the model.
      * @return A matrix with the weights for a polynomial regression model based on the data set provided.
      * @throws IllegalArgumentException If the data does not have one sample for each label.
+     * @throws IllegalArgumentException If n is less than one.
+     * @see #gradDes(Matrix, Matrix, Matrix, int, double, Function[])
+     */
+    public static Matrix polyGradDes(Matrix x, Matrix y, Matrix w, int n, double a) {
+        if (x.getRows() != y.getRows()) {
+            throw new IllegalArgumentException("The data does not have one sample for each label!");
+        }
+
+        if (n < 1) {
+            throw new IllegalArgumentException("The polynomial's order must be equal to or greater than one!");
+        }
+
+        Function[] fncs = BasisFunctions.polynomials(n);
+
+        return gradDes(x, y, w, n, a, fncs);
+    }
+
+    //ToDo: Make the model only in the terms of fncs[]
+    /**
+     * Calculates the weights for a regression model based on the data set provided and the basis functions provided
+     * using gradient descent.
+     * <p>
+     * The weights will form the following polynomial function:
+     * w + wx + wy + wz + ... + wx^2 + wx^3 + wx^n + ... + wy^2 + wy^3 + ... + wy^n + wz^2 + wz^3 + ... + wz^n + ...
+     * </p>
+     *
+     * @param xOrg A matrix of data parameters.
+     * @param y A vector of data labels.
+     * @param w A vector of initial weight guesses.
+     * @param n The order of the polynomial.
+     * @param a The learning rate of the model.
+     * @param fncs The set of basis functions the data will be modeled with.
+     * @return A matrix with the weights for a polynomial regression model based on the data set provided.
+     * @throws IllegalArgumentException If the data does not have one sample for each label.
      * @throws IllegalArgumentException If there is not one weight for each parameter.
      * @throws IllegalArgumentException If n is less than 1.
+     * @throws IllegalArgumentException If the number of basis functions is not equal to n + 1.
      */
-    public static Matrix polyGradDes(Matrix xOrg, Matrix y, Matrix w, int n, double a) {
+    public static Matrix gradDes(Matrix xOrg, Matrix y, Matrix w, int n, double a, Function[] fncs) {
         if (xOrg.getRows() != y.getRows()) {
             throw new IllegalArgumentException("The data does not have one sample for each label!");
         }
@@ -80,33 +119,32 @@ public class LinReg {
             throw new IllegalArgumentException("The polynomial's order must be equal to or greater than 1!");
         }
 
-        Matrix x;
-
-        try {
-            x = (Matrix) xOrg.clone();
-        } catch (CloneNotSupportedException e) {
-            throw new RuntimeException(e);
+        if (fncs.length != n + 1) {
+            throw new IllegalArgumentException("There must be exactly " + (n + 1) + " basis functions!");
         }
 
-        //Setting up x^n's
-        int numVars = x.getCols();
-        Matrix xn;
-        Matrix xTemp;
+        Matrix x = new Matrix (xOrg.getRows(), 1);
 
+        for (int i = 1; i <= x.getRows(); i++) {
+            x.setValue(i, 1, (Double) fncs[0].apply(xOrg.getValue(i, 1)));
+        }
+
+        int numVars = xOrg.getCols();
+        Matrix xn;
+
+
+        //Adding the basis functions to the matrix
         for (int i = 1; i <= numVars; i++) {//For each variable
-            for (int j = 2; j <= n; j++) {//For each polynomial order
-                xn = LinearAlgebra.vectorFromColumn(x, i);
-                xTemp = xn;
-                for (int k = 1; k < j; k++) {
-                    xn = LinearAlgebra.multiplyValues(xn, xTemp);
-                }
+            for (int j = 1; j <= n; j++) {//For each function order
+                xn = LinearAlgebra.vectorFromColumn(xOrg, i);
+                xn = LinearAlgebra.applyFunction(xn, fncs[j]);
                 x.addColRight(xn.getMatrix());
             }
         }
 
-        //For bias term
-        Matrix ones = LinearAlgebra.constantMatrix(x.getRows(), 1, 1.0);
-        x.addColLeft(ones.getMatrix());
+        //Should I always include a bias term, even when modeling data without polynomials?
+//        Matrix ones = LinearAlgebra.constantMatrix(x.getRows(), 1, 1.0);
+//        x.addColLeft(ones.getMatrix());
 
         Matrix dw = LinearAlgebra.constantMatrix(w.getRows(), 1, 5.0);
 
