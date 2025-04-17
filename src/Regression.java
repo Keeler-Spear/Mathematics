1b3908401c2b82a7fb85022a3a731cebef2a22de
Keeler Spear <keelerspear2@gmail.com>
2025-04-17 11:35:33 -0600

    Commit

--- src/Regression.java
+++ src/Regression.java
@@ -202,6 +202,7 @@ public class Regression {
     }
 
     //ToDo: Multi-class classification. I could decompose a multi-class classifier into multiple binary ones or use the other method discussed
+    //ToDo: Let the user pass an activation function as a parameter
     /**
      * Calculates the weights for a logistic regression model based on the data set provided and the basis functions provided
      * using gradient descent.
@@ -267,6 +268,80 @@ public class Regression {
         else {
             while (LinearAlgebra.l1Norm(dw) > TOL && i < MAX_ITERATIONS) {
                 dw = findDw(x, y, LinearAlgebra.roundMatrix(LinearAlgebra.applyFunction(LinearAlgebra.multiplyMatrices(x, w), sigmoid)));
+                w = LinearAlgebra.addMatrices(w, dw, -a); //x = x - a * dw
+                i++;
+            }
+        }
+
+        return w;
+    }
+
+    /**
+     * Calculates the weights for a classification model based on the data set, basis functions, and activation function provided
+     * using gradient descent.
+     * <p>
+     * The weights will form the following function:
+     * f[w + w1f(x) + wf2(x) + .. + wfn(x) + w1f(y) + wf2(y) + .. + wfn(y) + ...]
+     * </p>
+     * <p>
+     * In its current state, this method does have a few limitations. First, for very large xOrg data, the algorithm
+     * will diverge if the learning rate is not small enough. Second, it can only handle data with a single label.
+     * In the future, I plan to expand the algorithm to cover this. Third, it cannot handle functions with products,
+     * quotients, compositions, etc. of multiple variables, such as z = x * y or w = y * w^2 * cos(x).
+     * </p>
+     *
+     * @param xOrg A matrix of data parameters.
+     * @param y A vector of data labels.
+     * @param w A vector of initial weight guesses.
+     * @param a The learning rate of the model.
+     * @param activationFunction The activation function to be used.
+     * @param fncs The set of basis functions the data will be modeled with.
+     * @return A matrix with the weights for a logistic regression model based on the data set provided.
+     * @throws IllegalArgumentException If the data does not have one sample for each label.
+     * @throws IllegalArgumentException If there is not one weight for each parameter.
+     */
+    public static Matrix classificationReg(Matrix xOrg, Matrix y, Matrix w, double a, Function<Double, Double> activationFunction, Function[] fncs, boolean rounding) {
+        if (xOrg.getRows() != y.getRows()) {
+            throw new IllegalArgumentException("The data does not have one sample for each label!");
+        }
+
+        if (w.getRows() != (fncs.length - 1) * xOrg.getCols() + 1) {
+            throw new IllegalArgumentException("There must be one weight for each parameter!");
+        }
+
+        Matrix x = new Matrix (xOrg.getRows(), 1);
+
+        for (int i = 1; i <= x.getRows(); i++) {
+            x.setValue(i, 1, (Double) fncs[0].apply(xOrg.getValue(i, 1)));
+        }
+
+        int numVars = xOrg.getCols();
+        Matrix xn;
+
+        //Adding the basis functions to the matrix
+        for (int i = 1; i <= numVars; i++) {//For each variable
+            for (int j = 1; j < fncs.length; j++) {//For each function order
+                xn = LinearAlgebra.vectorFromColumn(xOrg, i);
+                xn = LinearAlgebra.applyFunction(xn, fncs[j]);
+                x.addColRight(xn.getMatrix());
+            }
+        }
+
+        Matrix dw = LinearAlgebra.constantMatrix(w.getRows(), 1, 5.0);
+
+        //Gradient descent
+        int i = 0;
+        if (!rounding) {
+            while (LinearAlgebra.l1Norm(dw) > TOL && i < MAX_ITERATIONS) {
+                dw = findDw(x, y, LinearAlgebra.applyFunction(LinearAlgebra.multiplyMatrices(x, w), activationFunction));
+                w = LinearAlgebra.addMatrices(w, dw, -a); //x = x - a * dw
+                i++;
+            }
+        }
+
+        else {
+            while (LinearAlgebra.l1Norm(dw) > TOL && i < MAX_ITERATIONS) {
+                dw = findDw(x, y, LinearAlgebra.roundMatrix(LinearAlgebra.applyFunction(LinearAlgebra.multiplyMatrices(x, w), activationFunction)));
                 w = LinearAlgebra.addMatrices(w, dw, -a); //x = x - a * dw
                 i++;
             }
