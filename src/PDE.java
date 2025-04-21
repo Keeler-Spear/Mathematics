401af39593a68732e91cb3ebd8bdbf737cbbd38e
Keeler Spear <keelerspear2@gmail.com>
2025-04-20 20:56:56 -0600

    Added numerical methods for the transport equation

--- src/PDE.java
+++ src/PDE.java
@@ -1,3 +1,4 @@
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 /**
@@ -48,6 +49,137 @@ public class PDE {
         }
 
         return new Matrix(vals);
+    }
+
+    /**
+     * Solves the transport equation (ut + cux = 0) analytically.
+     *
+     * @param c The coefficient of the ux term.
+     * @param f The initial condition for the PDE.
+     * @return A function that is a solution to the transport equation.
+     */
+    public static BiFunction<Double, Double, Double> solveTrEq(double c, Function<Double, Double> f) {
+        BiFunction<Double, Double, Double> sol = (x, t) -> f.apply(x - c * t);
+        return sol;
+    }
+
+    /**
+     * Solves the transport equation (ut + cux = 0) numerically with FTFS.
+     *
+     * @param c The coefficient of the ux term.
+     * @param f The initial condition for the PDE.
+     * @param x0 The starting spacial position.
+     * @param xMax The ending spacial position.
+     * @param t0 The starting temporal position
+     * @param tMax The ending temporal position.
+     * @param h The space step to be used.
+     * @param k The time step to be used.
+     * @return A function that is a solution to the transport equation.
+     */
+    public static Matrix solveTrEqFTFS(double c, Function<Double, Double> f, double x0, double xMax, double t0, double tMax, double h, double k) {
+        double lambda = c * k / h;
+        int iterations = (int) Math.round((tMax - t0) / k);
+
+        //Building v0
+        Matrix v0 = LinearAlgebra.linSpace(x0, xMax, h);
+        int size = v0.getRows();
+        v0 = LinearAlgebra.applyFunction(v0, f);
+
+        //Building A
+        Matrix A = new Matrix(size, size);
+        for (int i = 1; i <= size; i++) {
+            A.setValue(i, i, 1 + lambda);
+        }
+        for (int i = 1; i < size; i++) {
+            A.setValue(i, i + 1, -lambda);
+        }
+
+        //Iterating the system in time
+        return iterateSystem(A, v0, iterations);
+    }
+
+    /**
+     * Solves the transport equation (ut + cux = 0) numerically with FTBS.
+     *
+     * @param c The coefficient of the ux term.
+     * @param f The initial condition for the PDE.
+     * @param x0 The starting spacial position.
+     * @param xMax The ending spacial position.
+     * @param t0 The starting temporal position
+     * @param tMax The ending temporal position.
+     * @param h The space step to be used.
+     * @param k The time step to be used.
+     * @return A function that is a solution to the transport equation.
+     */
+    public static Matrix solveTrEqFTBS(double c, Function<Double, Double> f, double x0, double xMax, double t0, double tMax, double h, double k) {
+        double lambda = c * k / h;
+        int iterations = (int) Math.round((tMax - t0) / k);
+
+        //Building v0
+        Matrix v0 = LinearAlgebra.linSpace(x0, xMax, h);
+        int size = v0.getRows();
+        v0 = LinearAlgebra.applyFunction(v0, f);
+
+        //Building A
+        Matrix A = new Matrix(size, size);
+        for (int i = 1; i <= size; i++) {
+            A.setValue(i, i, 1 - lambda);
+        }
+        for (int i = 2; i <= size; i++) {
+            A.setValue(i, i - 1, lambda);
+        }
+
+        //Iterating the system in time
+        return iterateSystem(A, v0, iterations);
+    }
+
+    /**
+     * Solves the transport equation (ut + cux = 0) numerically with FTCS.
+     *
+     * @param c The coefficient of the ux term.
+     * @param f The initial condition for the PDE.
+     * @param x0 The starting spacial position.
+     * @param xMax The ending spacial position.
+     * @param t0 The starting temporal position
+     * @param tMax The ending temporal position.
+     * @param h The space step to be used.
+     * @param k The time step to be used.
+     * @return A function that is a solution to the transport equation.
+     */
+    public static Matrix solveTrEqFTCS(double c, Function<Double, Double> f, double x0, double xMax, double t0, double tMax, double h, double k) {
+        double lambda = c * k / (2 * h);
+        int iterations = (int) Math.round((tMax - t0) / k);
+
+        //Building v0
+        Matrix v0 = LinearAlgebra.linSpace(x0, xMax, h);
+        int size = v0.getRows();
+        v0 = LinearAlgebra.applyFunction(v0, f);
+
+        //Building A
+        Matrix A = new Matrix(size, size);
+        for (int i = 1; i <= size; i++) {
+            A.setValue(i, i, 1);
+        }
+        for (int i = 2; i <= size; i++) {
+            A.setValue(i, i - 1, lambda);
+        }
+        for (int i = 1; i < size; i++) {
+            A.setValue(i, i + 1, -lambda);
+        }
+
+        //Iterating the system in time
+        return iterateSystem(A, v0, iterations);
+    }
+
+    private static Matrix iterateSystem(Matrix A, Matrix v, int n) {
+        Matrix sol = LinearAlgebra.transpose(v);
+
+        for (int i = 0; i < n; i++) {
+            v = LinearAlgebra.multiplyMatrices(A, v);
+            sol.addRowBottom(v.getMatrix());
+        }
+
+        return sol;
     }
 
 }
