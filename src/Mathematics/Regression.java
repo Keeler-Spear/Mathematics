4421870759f52979b9c1dab338668e52697c6cc4
Keeler Spear <keelerspear2@gmail.com>
2025-07-05 15:27:32 -0600

    Update Regression.java

--- src/Mathematics/Regression.java
+++ src/Mathematics/Regression.java
@@ -4,9 +4,6 @@ import java.util.function.Function;
 
 /**
  * A static class that performs regression operations via numerical methods.
- * <p>
- *     This class and all functions within were added after I submitted my application to your institution.
- * </p>
  *
  * @author Keeler Spear
  * @version %I%, %G%
